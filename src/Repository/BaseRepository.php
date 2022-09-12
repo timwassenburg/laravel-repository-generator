eb5ea32251a72596b921d31397b5a6e01c6bc257
NyirendaJr <javaxynyirenda@gmail.com>
2022-09-12 17:14:30 +0300

    added find with, find with trash, delete and update methods in the base repository

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -41,11 +41,46 @@ class BaseRepository implements EloquentRepositoryInterface
     }
 
     /**
-     * @param $id
+     * @param int $id
+     * @param array $with
+     * @param array $params
      * @return Model
      */
-    public function find($id): ?Model
+    public function find(int $id, array $with = [], array $params = []): ?Model
     {
-        return $this->model->find($id);
+        return $this->model->with($with)->findOrFail($id);;
+    }
+
+
+    /**
+     * @param int $id
+     * @return Model|null
+     */
+    public function findWithTrash(int $id): ?Model
+    {
+        return $this->model::query()->withTrashed()->find($id);
+    }
+
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function delete(int $id)
+    {
+        return $this->model::query()->find($id)->delete();
+    }
+
+
+    /**
+     * @param int $id
+     * @param array $attributes
+     * @return Model
+     */
+    public function update(int $id, array $attributes): Model
+    {
+        $model = $this->find($id);
+
+        return $model->update($attributes);
     }
 }
